4ee4b6632c1888ef0692c4aff0546806477b27f9
Sheila O'Donnell <sheilacodonnell@gmail.com>
2020-05-04 12:09:28 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent';
+import MouseComponent from './MouseComponent';
+import EinsteinQuoteComponent from './EinsteinQuoteComponent';
 
 class App extends Component {
 	render() {
@@ -6,8 +9,8 @@ class App extends Component {
 		return (
 			<div className="App">
 				<CatComponent />
-				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<EinsteinQuoteComponent />
+				<MouseComponent />
 			</div>
 		);
 	}
---
+++ src/EinsteinQuoteComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EinsteinQuoteComponent extends Component {
+    render() {
+      return (
+        <div className="bar" id="einstein">
+          Some cool quote
+        </div>
+      );
+    }
+  } 
